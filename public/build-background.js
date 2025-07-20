c583e1a67622e340028ca14bcd053f1d4b2875e9
chris tabb <christabb@gmail.com>
2025-07-20 13:25:05 -0800

    Updates to the bundler

--- public/build-background.js
+++ public/build-background.js
@@ -4,9 +4,9 @@ esbuild.build({
   entryPoints: ['public/background.js'],
   bundle: true,
   outfile: 'public/dist/background.js',
-  platform: 'neutral',           // no node/browser specific shims
-  format: 'iife',                // immediately invoked function expression
+  platform: 'neutral',
+  format: 'iife',
   minify: true,
   sourcemap: true,
-  external: ['@supabase/supabase-js'],  // exclude Supabase since loaded via CDN
+  external: ['@supabase/supabase-js'],
 }).catch(() => process.exit(1));
