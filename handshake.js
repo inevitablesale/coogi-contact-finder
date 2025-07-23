02c5c4ba77292edbd624b7e8a6d3ee70e8a79082
inevitablesale <chris@inevitable.sale>
2025-07-23 12:51:11 -0800

    Update handshake.js

--- handshake.js
+++ handshake.js
@@ -1,13 +1,11 @@
 // handshake.js
 console.log("Coogi Extension: Handshake content script injecting page messenger.");
 
-// Inject the extension ID into the DOM so the web app can access it.
+// Expose the extension ID to the page
 document.body.setAttribute('data-coogi-extension-id', chrome.runtime.id);
 
+// Inject page-messenger.js into the page
 const script = document.createElement('script');
-// Get the URL of the messenger script from the extension's resources.
 script.src = chrome.runtime.getURL('page-messenger.js');
-// Append the script to the document's head.
 (document.head || document.documentElement).appendChild(script);
-// The script will load and execute, so we can remove the tag from the DOM.
-script.remove();
+script.onload = () => script.remove();
